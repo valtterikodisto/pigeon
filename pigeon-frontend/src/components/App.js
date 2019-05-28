3c7a90c794c5fb3868e34183b1f3b6208214548e
uberballo <kasper.paavolainen@helsinki.fi>
2019-05-29 01:13:34 +0300

    Fixing my own mistake

--- pigeon-frontend/src/components/App.js
+++ pigeon-frontend/src/components/App.js
@@ -4,31 +4,7 @@ import Chat from './chat/Chat'
 import './App.css'
 
 const App = props => {
-  return (
-<<<<<<< HEAD
-    /*
-    Tätä ehdottaisin. Nimet ovat aika itsestään selviä.
-    App.css sitten järjestelee nuo elementit.
-    <div id="container">
-      <Sidebar />
-      <section id="main">
-        <MessagesList />
-        <AddMessage />
-      </section>
-    </div>
-    */
-    <div className="container">
-      <Sidebar users={users} />
-      <div className="main">
-        <MessageWindow messages={messages} />
-        <AddMessage />
-      </div>
-    </div>
-=======
-    // Chat-scene
-    <Chat />
->>>>>>> a22ed3bd8cdb202cdcc7d70eac597bf510b2e6cb
-  )
+  return <Chat />
 }
 
 export default App
