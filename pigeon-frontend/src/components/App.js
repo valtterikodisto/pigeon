d635e2e3ba1bfe278b9d75ab5961b23f1dd86fdf
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-05-29 00:42:57 +0300

    Cleaned App.js, moved code to Chat component

--- pigeon-frontend/src/components/App.js
+++ pigeon-frontend/src/components/App.js
@@ -1,46 +1,12 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 
-import groupService from '../services/groups'
-
-import MessageWindow from './MessageWindow'
-import AddMessage from './AddMessage'
-import Sidebar from './Sidebar'
+import Chat from './chat/Chat'
 import './App.css'
 
 const App = props => {
-  const [messages, setMessages] = useState([])
-  const [users, setUsers] = useState([])
-  useEffect(() => {
-    groupService
-      .get()
-      .then(group => {
-        console.log(group.messages)
-        setUsers(group.users)
-        setMessages(group.messages)
-      })
-      .catch(error => console.log('Error when tried to get group'))
-  }, [])
-
   return (
-    /*
-    Tätä ehdottaisin. Nimet ovat aika itsestään selviä.
-    App.css sitten järjestelee nuo elementit.
-    <div id="container">
-      <Sidebar />
-      <section id="main">
-        <MessagesList />
-        <AddMessage />
-      </section>
-    </div>
-    */
-    <div className="container">
-      <Sidebar users={users} />
-      <div className="main">
-        <p>App</p>
-        <MessageWindow messages={messages} />
-        <AddMessage />
-      </div>
-    </div>
+    // Chat-scene
+    <Chat />
   )
 }
 
