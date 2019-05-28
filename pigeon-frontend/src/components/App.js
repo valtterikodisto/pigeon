d3b5449ad7727a3b03f253fb463382381cc9a9dd
uberballo <kasper.paavolainen@helsinki.fi>
2019-05-28 20:04:46 +0300

    Added initial template and .css file for App.js

--- pigeon-frontend/src/components/App.js
+++ pigeon-frontend/src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 const App = props => {
-  return <p>App</p>
+  return (
+    /*
+    Tätä ehdottaisin. Nimet ovat aika itsestään selviä.
+    App.css sitten järjestelee nuo elementit.
+    <div id="container">
+      <Sidebar />
+      <section id="main">
+        <MessagesList />
+        <AddMessage />
+      </section>
+    </div>
+    */
+    <p>App</p>
+  )
 }
 
 export default App
