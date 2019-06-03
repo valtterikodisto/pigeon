06395d3ed8957be7f731625236fc9aa4dc68ee22
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-06-03 21:40:36 +0300

    Added logo to login page

--- pigeon-frontend/src/components/login/Login.js
+++ pigeon-frontend/src/components/login/Login.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
 import './Login.css'
+import logo from '../../pictures/pigeon-logo.svg'
 
 const Login = () => {
   const [loginHidden, setLoginHidden] = useState(false)
@@ -21,7 +22,10 @@ const Login = () => {
   return (
     <div className="login-page-wrapper">
       <header>
-        <div className="logo-wrapper">Pigeon</div>
+        <div className="logo-wrapper">
+          <img src={logo} alt="pigeon logo" />
+          <p className="logo-text">Pigeon</p>
+        </div>
         <div className="login-register-button-wrapper">
           <div className="login-register-button">
             <button
