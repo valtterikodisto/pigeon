e4e7b003b6c2114e1180d9c022d442e2fbdb6832
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-06-04 22:46:38 +0300

    Added password field to User model

--- pigeon-backend/models/message.js
+++ pigeon-backend/models/message.js
@@ -1,18 +1,17 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    sender: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    timestapn: {
-        type: Date,
-
-    },
-    message: {
-        type: String,
-    }
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  timestapn: {
+    type: Date
+  },
+  message: {
+    type: String
+  }
 })
 
 module.exports = mongoose.model('Message', schema)
--- pigeon-backend/models/user.js
+++ pigeon-backend/models/user.js
@@ -1,25 +1,30 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        minglength: 3
-    },
-    firstName: {
-        type: String,
-        required: true,
-        minglength: 3
-    },
-    lastName: {
-        type: String,
-        required: true,
-        minglength: 3
-    },
-    lastSeen: {
-        type: Date
-    },
-    //groups
+  username: {
+    type: String,
+    required: true,
+    minlength: 3
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minglength: 3
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minglength: 3
+  },
+  lastSeen: {
+    type: Date
+  }
+  //groups
 })
 
 module.exports = mongoose.model('User', schema)
