b7e93542a8ee21f821ce4c0cf123db4afb07c8c4
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-06-05 13:26:42 +0300

    Added user authentication and password hashing

--- pigeon-backend/models/user.js
+++ pigeon-backend/models/user.js
@@ -4,6 +4,7 @@ const schema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
     minlength: 3
   },
   password: {
@@ -30,15 +31,6 @@ const schema = new mongoose.Schema({
       ref: 'Group'
     }
   ]
-})
-
-schema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-    delete returnedObject.password
-  }
 })
 
 module.exports = mongoose.model('User', schema)
--- pigeon-backend/server.js
+++ pigeon-backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const { ApolloServer, UserInputError, gql } = require('apollo-server')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const User = require('./models/user')
 const Message = require('./models/message')
@@ -10,15 +12,21 @@ const Group = require('./models/group')
 mongoose.set('useFindAndModify', false)
 
 const MONGODB_URI = process.env.MONGODB_URI
+const JWT_SECRET = process.env.SECRET
 
 //TODO, move to the file typedefs in the folder graphql
 //Add all required fields to User and Message
 const typeDefs = gql`
   type User {
     username: String!
-    password: String!
     firstName: String!
     lastName: String!
+    groups: [Group]
+    id: ID!
+  }
+
+  type Token {
+    value: String!
   }
 
   type Message {
@@ -33,12 +41,14 @@ const typeDefs = gql`
   }
 
   type Query {
+    currentUser: User
     allUsers: [User]!
     allMessages: [Message]!
   }
 
   type Mutation {
     addUser(username: String!, password: String!, firstName: String!, lastName: String!): User
+    login(username: String!, password: String!): Token
     addMessage(username: String, message: String): Message
   }
 `
@@ -48,6 +58,9 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
+    currentUser: (root, args, context) => {
+      return context.currentUser
+    },
     allUsers: (root, args) => {
       return User.find({})
     },
@@ -57,7 +70,9 @@ const resolvers = {
   },
   Mutation: {
     addUser: async (root, args) => {
-      const newUser = new User({ ...args })
+      const saltRounds = 10
+      const password = await bcrypt.hash(args.password, saltRounds)
+      const newUser = new User({ ...args, password })
       console.log(newUser)
 
       try {
@@ -69,6 +84,22 @@ const resolvers = {
       }
 
       return newUser
+    },
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+      const authSuccessful =
+        user === null ? false : await bcrypt.compare(args.password, user.password)
+
+      if (!authSuccessful) {
+        throw new UserInputError('Invalid username or password')
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id
+      }
+
+      return { value: jwt.sign(userForToken, JWT_SECRET) }
     },
     addMessage: async (root, args) => {
       const user = await User.findOne({ username: args.username })
@@ -105,7 +136,16 @@ mongoose
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toUpperCase().startsWith('BEARER ')) {
+      const decodedToken = jwt.verify(auth.substring(7), JWT_SECRET)
+      const currentUser = await User.findById(decodedToken.id)
+
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
