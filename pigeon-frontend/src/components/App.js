30af72c14ea62dc8aa5cf42ef1784037e4f954b2
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-07-01 15:32:35 +0300

    Added current user to App component

--- pigeon-frontend/src/components/App.js
+++ pigeon-frontend/src/components/App.js
@@ -7,17 +7,35 @@ import './App.css'
 import './App.scss'
 
 const App = () => {
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('current-user'))
   const [token, setToken] = useState(localStorage.getItem('pigeon-token'))
   const [chatId, setChatId] = useState(null)
   const changeToken = token => setToken(token)
+
+  console.log('current user', currentUser)
 
   if (!token) {
     return <Login token={token} setToken={changeToken} />
   }
   if (!chatId) {
-    return <Profile token={token} setToken={setToken} setChatId={setChatId} />
+    return (
+      <Profile
+        token={token}
+        setToken={setToken}
+        setChatId={setChatId}
+        setCurrentUser={setCurrentUser}
+      />
+    )
   }
-  return <Chat token={token} setToken={setToken} chatId={chatId} setChatId={setChatId} />
+  return (
+    <Chat
+      token={token}
+      setToken={setToken}
+      chatId={chatId}
+      setChatId={setChatId}
+      currentUser={currentUser}
+    />
+  )
 }
 
 export default App
